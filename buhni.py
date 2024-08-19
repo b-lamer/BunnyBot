8953cdc02b579e50ec1fc7c62dc5ced8f492732d
b-lamer <152201467+b-lamer@users.noreply.github.com>
2024-08-19 15:15:00 -0500

    testing limits of free version
    (there's a lot of limits)

--- buhni.py
+++ buhni.py
@@ -1,5 +1,15 @@
 import tweepy
+import sys
+
 from credentials import twitter_bot_keys
 auth = tweepy.OAuthHandler(twitter_bot_keys['API Key'], twitter_bot_keys['API Key Secret'])
 auth.set_access_token(twitter_bot_keys['Access Token'], twitter_bot_keys['Access Token Secret'])
 api = tweepy.API(auth)
+
+try:
+    api.verify_credentials()
+    print("Authentication OK\n")
+except:
+    print("Error during authentication\n")
+
+print(api.search_tweets("carrot"))
