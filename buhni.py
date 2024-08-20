1b1915436ca0f5f893a40dabc78e132c7d2a0796
b-lamer <152201467+b-lamer@users.noreply.github.com>
2024-08-20 14:07:02 -0500

    Update buhni.py

--- buhni.py
+++ buhni.py
@@ -3,3 +3,4 @@ from credentials import twitter_bot_keys
 auth = tweepy.OAuthHandler(twitter_bot_keys['API Key'], twitter_bot_keys['API Key Secret'])
 auth.set_access_token(twitter_bot_keys['Access Token'], twitter_bot_keys['Access Token Secret'])
 api = tweepy.API(auth)
+
