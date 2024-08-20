d037e64425a94e3285329367f87f79aec752e117
b-lamer <152201467+b-lamer@users.noreply.github.com>
2024-08-20 14:23:43 -0500

    Trial version
    Apparently I was using API v1.1... Fingers crossed this works :)

--- buhni.py
+++ buhni.py
@@ -1,6 +1,14 @@
 import tweepy
 from credentials import twitter_bot_keys
-auth = tweepy.OAuthHandler(twitter_bot_keys['API Key'], twitter_bot_keys['API Key Secret'])
-auth.set_access_token(twitter_bot_keys['Access Token'], twitter_bot_keys['Access Token Secret'])
-api = tweepy.API(auth)
 
+client = tweepy.Client(bearer_token=twitter_bot_keys['Bearer token'])
+
+query = "Carrot"
+
+tweets = client.search_recent_tweets(query=query, max_results=10)
+
+if tweets.data:
+    for tweet in tweets.data:
+        print(f"Tweet: {tweet.text}")
+else:
+    print("No tweets found.")
